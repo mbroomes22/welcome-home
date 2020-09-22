f337ac3049b268822578317529138840de93ed69
Michelle <michelle_broomes@yahoo.com>
2020-09-22 01:58:14 -0400

    working protootype app

--- App.js
+++ App.js
@@ -1,31 +1,156 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import Swiper from 'react-native-deck-swiper'
+import Zillow from './Zillow'
+import React, {useState, createRef} from 'react';
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+import { StyleSheet, Text, View, Image, StatusBar, Dimensions} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Transition, Transitioning } from 'react-native-reanimated';
+import { useFonts, Inter_900Black} from '@expo-google-fonts/inter';
+
+
+const {width} = Dimensions.get('window')
+
+const colors = {
+  red: "#ec2379",
+  blue: "#0070ff",
+  gray: "#777777",
+  black: "#000000",
+  white: "#ffffff"
+}
+
+const ANIMATION_DURATION = 200;
+
+const transition = (
+  <Transition.Sequence>
+    <Transition.Out type='slide-bottom' durationMs={ANIMATION_DURATION} interpolation='easeIn'/>
+    <Transition.Together>
+      <Transition.In type='fade' durationMs={ANIMATION_DURATION} delayMs={ANIMATION_DURATION / 2}/>
+      <Transition.In type='slide-bottom' durationMs={ANIMATION_DURATION} delayMs={ANIMATION_DURATION / 2} interpolation='easeOut'/>
+    </Transition.Together>
+  </Transition.Sequence>
+)
+
+const Card = ({card}) => (
+  <View style={styles.card}>
+    <Image source={{uri: card.image}} style={styles.cardImage} />
+  </View>
+)
+
+const CardDetails = ({index}) => {
+  return (
+  <View style={styles.CardDetails} key={Zillow[index].id}>
+    <Text style={[styles.text, styles.address]}>{Zillow[index].address}</Text>
+    <Text style={[styles.text, styles.price]}>${Zillow[index].price}</Text>
+    <Text style={[styles.text, styles.beds]}>{Zillow[index].beds} bed</Text>
+    <Text style={[styles.text, styles.baths]}>{Zillow[index].baths} bath</Text>
+  </View>)
+}
+
+const swiperRef = createRef()
+const transitionRef = createRef()
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("")
-  const [courseGoals, setCourseGoals] = useState([])
-
-  const goalInputHandler = (enteredGoal) => {
-    setEnteredGoal(enteredGoal)
+  const [index, setIndex] = useState(0)
+  const onSwiped = () => {
+    transitionRef.current.animateNextTransition()
+    setIndex((index + 1) % Zillow.length)
   }
-
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
-  }
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
 
   return (
     <View style={styles.container}>
-      <View style={styles.textAndBtn} >
-        <TextInput placeholder="course goal" 
-        style={styles.text} 
-        onChangeText={goalInputHandler} 
-        value={enteredGoal} 
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
+      <View style={styles.logoP}>
+      <Text style={styles.logo}>
+      Next Nest
+      </Text>
       </View>
-      <View >
-        {courseGoals}
+      <StatusBar hidden/>
+      <MaterialCommunityIcons 
+        name='crop-square'
+        size={width}
+        color={colors.blue}
+        style={styles.diamondIcon}
+      />
+      <View style={styles.swiperContainer}>
+      <Swiper
+      ref={swiperRef}
+      cards={Zillow}
+      cardIndex={index}
+      renderCard={(card) => <Card card={card}/>}
+      onSwiped={onSwiped}
+      stackSize={4}
+      stackScale={10}
+      stackSeparation={14}
+      disableTopSwipe
+      disableBottomSwipe
+      animateOverlayLabelsOpacity
+      animateCardOpacity
+      infinite
+      backgroundColor={'transparent'}
+      overlayLabels={{
+        left: {
+          title: "NOPE",
+          style: {
+            label: {
+              backgroundColor: colors.red,
+              color: colors.white,
+              fontSize: 24
+            },
+            wrapper: {
+              flexDirection: "column",
+              alignItems: "flex-end",
+              justifyContent: "flex-start",
+              marginTop: 20,
+              marginLeft: -20
+            }
+          }
+        },
+        right: {
+          title: "LIKE",
+          style: {
+            label: {
+              backgroundColor: colors.blue,
+              color: colors.white,
+              fontSize: 24
+            },
+            wrapper: {
+              flexDirection: "column",
+              alignItems: "flex-start",
+              justifyContent: "flex-start",
+              marginTop: 20,
+              marginLeft: 20
+            }
+        }}
+      }}
+      />
+      </View>
+      <View style={styles.bottomContainer}>
+        <Transitioning.View ref={transitionRef} transition={transition}>
+        <CardDetails index={index}/>
+        </Transitioning.View>
+        <View style={styles.bottomButtonsContainer}>
+          <MaterialCommunityIcons.Button
+            name = "close"
+            size = {94}
+            backgroundColor= 'transparent'
+            underlayColor= 'transparent'
+            activeOpacity={0.3}
+            color={colors.red}
+            onPress={()=> swiperRef.current.swipeLeft()}
+          />
+          <MaterialCommunityIcons.Button
+            name = "check"
+            size = {94}
+            backgroundColor= 'transparent'
+            underlayColor= 'transparent'
+            activeOpacity={0.3}
+            color={colors.blue}
+            onPress={()=> swiperRef.current.swipeRight()}
+          />
+        </View>
       </View>
     </View>
   );
@@ -34,23 +159,82 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: colors.white,
+  },
+
+  card: {
+    flex: 0.45,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: colors.black,
+    shadowOpacity: .08,
+    shadowOffset: {width: 0, height: 0},
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.white
   },
 
-  textAndBtn: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    margin: 15
+  cardImage: {
+    width: "75%",
+    flex: 1,
+    resizeMode: 'contain'
   },
 
+  swiperContainer: {
+    flex:0.55,
+  },
+
+  bottomContainer: {
+    flex: 0.45,
+    justifyContent: 'space-evenly'
+  },
+
+  bottomButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+
+  CardDetails: {
+    alignItems: 'center'
+  },
   text: {
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 10,
-    padding:10,
-    width: '60%'
+    fontFamily: 'Courier'
+  },
+  logo: {
+    color: colors.blue,
+    fontSize: 40,
+    fontFamily: 'Inter_900Black'
+  },
+  logoP: {
+    top: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  address: {
+    fontSize: 24,
+    marginBottom: 10,
+    color: colors.gray
+  },
+  price: {
+    color: colors.blue,
+    fontSize: 22,
+    fontWeight: '500'
+  },
+  beds: {
+    color: colors.blue
+  },
+  baths: {
+    color: colors.blue
+  },
+  diamondIcon: {
+    opacity: 0.03,
+    transform: [{
+      rotate: '45deg'
+    }, {
+      scale:1.6
+    }],
+    position: 'absolute',
+    top: 30,
+    left: -20
   }
 });
---
+++ Zillow.js
@@ -0,0 +1,45 @@
+export default [
+    {
+        id: "1",
+        price: "1500",
+        address: "10 Greene st, Jersey City, NJ",
+        image: "https://ds1.cityrealty.com/img/1655f4e3904fb79cb987ab7755d2b3f4b8f37f88+736++0+60/1-city-point.jpg",
+        beds: "1",
+        baths: "1"
+    },{
+        id: "2",
+        price: "2500",
+        address: "110 1st st, Jersey City, NJ",
+        image: "https://images1.apartments.com/i2/OYexjpsYBEI-DceI1wLgqdsPpAz-Zr-v7LOmvadrWwQ/111/the-one-jersey-city-nj-living-room.jpg",
+        beds: "1",
+        baths: "1"
+    },{
+        id: "3",
+        price: "1800",
+        address: "100 Clifton Pl, Jersey City, NJ",
+        image: "https://images1.apartments.com/i2/-WUE0ZLE5kTGgkGXad1fbsMKo_cC7qnW6yq0mYszVw0/111/the-beacon-jersey-city-nj-primary-photo.jpg",
+        beds: "1",
+        baths: "1"
+    },{
+        id: "4",
+        price: "2300",
+        address: "25 Park Ln S, Jersey City, NJ",
+        image: "https://www.brickunderground.com/sites/default/files/styles/blog_primary_image/public/blog/images/Ellipse%20two%20bedroom.jpeg",
+        beds: "1",
+        baths: "1"
+    },{
+        id: "5",
+        price: "2100",
+        address: "2 2nd st, Jersey City, NJ",
+        image: "https://28nwgk2wx3p52fe6o9419sg5-wpengine.netdna-ssl.com/wp-content/uploads/2019/01/jersey-city-apartments-for-rent-m2-marbella-2.jpg",
+        beds: "1",
+        baths: "1"
+    },{
+        id: "6",
+        price: "1700",
+        address: "1 2nd st, Jersey City, NJ",
+        image: "https://i.pinimg.com/originals/8b/22/88/8b2288bd1cd4b57c3ed3c88d4f2712cd.jpg",
+        beds: "1",
+        baths: "1"
+    }
+]
