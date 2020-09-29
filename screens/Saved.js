250271160b15721fc24341ef9ba6aff80a0f01a2
Michelle <michelle_broomes@yahoo.com>
2020-09-28 21:48:32 -0400

    added flatlist to saved screen

--- screens/Saved.js
+++ screens/Saved.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Image, StatusBar, Dimensions} from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, Dimensions, ScrollView, FlatList} from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 //show all 'liked' apts, able to view apt details
 
@@ -11,16 +12,34 @@ const colors = {
 export default function Saved(props) {
     const {savedApts} = props
     [contacted, setContacted] = useState([])
+
+    const pressHandler = (id) => {
+        console.log("id => ", id)
+        // setContacted([...contacted, id])
+        setContacted((prevContacted)=>{
+            return prevContacted.push(savedApts.filter(apt => apt.id === id))
+        })
+    }
+
     return (
         <View>
             <Text style={styles.heading}>Saved Homes</Text>
-            <View>
+            <FlatList 
+            numColumns={5}
+            keyExtractor={(apt) => apt.id}
+            data={savedApts}
+            renderItem={({apt})=> (
+                <TouchableOpacity onPress={() => pressHandler(apt.id)}>
+                <Image style={styles.imgScroll} source={apt.image[0]} />
+                </TouchableOpacity>
+            )}/>
+            {/* <ScrollView>
                 {savedApts.map(apt => 
                   <View key={apt.id} style={styles.imgScroll} onPress={contacted.push(apt)}>
-                      {apt.image[0]}
+                     <Image source={apt.image[0]} />
                   </View>  
                     )}
-            </View>
+            </ScrollView> */}
             <View>
                 {contacted.map(apt => 
                     <View key={apt.id}>
